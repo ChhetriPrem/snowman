53da8c0fffc3bf48f6732e6d82d146aa0d6c86d1
Renao Chetri <63331185+ChhetriPrem@users.noreply.github.com>
2024-05-14 23:24:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+
+var slides1 = [
+  "images/vanilla.png",
+  "images/chocolate.png",
+  "images/kesarpista.png",
+];
+var slides2 = [
+  "images/kesarpista.png",
+
+  "images/vanilla.png",
+  "images/chocolate.png",
+];
+var Start1 = 0;
+var Start2 = 0;
+
+function slider() {
+  if (Start1 < slides1.length) {
+    Start1 = Start1 + 1;
+  } else {
+    Start1 = 1;
+  }
+
+  if (Start2 < slides2.length) {
+    Start2 = Start2 + 1;
+  } else {
+    Start2 = 1;
+  }
+  console.log(img1);
+  img1.src = slides1[Start1 - 1];
+  img2.src = slides2[Start2 - 1];
+}
+setInterval(slider, 2000);
